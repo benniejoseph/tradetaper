b776963b79f206d5ceea807ef670e5085a53728d
bennierichard <berichard@deloitte.com>
2025-06-16 18:49:18 +0530

    Fix API URL configuration - use production backend by default

--- tradetaper-backend/src/websocket/websocket.module.ts
+++ tradetaper-backend/src/websocket/websocket.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TradesGateway } from './trades.gateway';
+import { WebSocketService } from './websocket.service';
 
 @Module({
-  providers: [TradesGateway],
-  exports: [TradesGateway],
+  providers: [WebSocketService, TradesGateway],
+  exports: [WebSocketService, TradesGateway],
 })
 export class WebSocketGatewayModule {}
